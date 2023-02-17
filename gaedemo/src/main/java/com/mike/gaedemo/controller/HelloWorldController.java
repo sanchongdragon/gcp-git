407eb5dbbd07f76cf8eec86f9eb9e6265ca418b0
Mike.Wu <Mike.Wu@tpisoftware.com>
2023-02-17 15:13:37 +0800

    add logger

--- gaedemo/src/main/java/com/mike/gaedemo/controller/HelloWorldController.java
+++ gaedemo/src/main/java/com/mike/gaedemo/controller/HelloWorldController.java
@@ -1,13 +1,18 @@
 package com.mike.gaedemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
+
     @GetMapping(value = "/")
     public String helloWorld(){
+        LOG.info("hello world method has been called!");
         return "Hello World!";
     }
 
